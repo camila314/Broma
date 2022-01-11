a45b1d0e3997afd3e11e665f064173b5742e107a
camden314 <camden7306@gmail.com>
2022-01-11 16:00:23 -0600

    interface for parsing files

--- src/broma.hpp
+++ src/broma.hpp
@@ -4,3 +4,17 @@
 #include "ast.hpp"
 #include "lex.hpp"
 #include "parse.hpp"
+#include <fstream>
+
+Root parseFile(char const* fname) {
+	ifstream in(fname);
+	string contents;
+	while (in)
+		in >> contents;
+
+	stringstream stream;
+	stream << contents;
+
+	vector<Token> t = lexStream(stream);
+	return parseTokens(t);
+}
