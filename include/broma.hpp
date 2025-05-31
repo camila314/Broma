5384e87de1fc7724db1b8ec1407d161db4d0cecb
camila <47485054+camila314@users.noreply.github.com>
2025-05-31 17:16:59 -0500

    Update broma.hpp for filesystem path

--- include/broma.hpp
+++ include/broma.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "ast.hpp"
+#include <filesystem>
 
 namespace broma {
 	/// @brief Pass a file for Broma to parse into an AST.
@@ -8,5 +9,5 @@ namespace broma {
 	/// This function throws, so make sure to handle errors properly if needed.
 	///
 	/// @param fname The path of the file you want to parse, as a string.
-	Root parse_file(std::string const& fname);
+	Root parse_file(std::filesystem::path const& fname);
 }
